d514edb7b843b50d675d92a6c0bb59f3b1573c6e
feliciacalming <felicia.calming@gmail.com>
2023-04-11 20:28:53 +0200

    (feat): add ShowGame.vue

---
+++ src/components/ShowGame.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+let id: number = 0;
+</script>
+
+<template>
+  <h3>Det är x tur</h3>
+  <div class="game-container">
+    <div v-for="index in 9" :key="index">{{ index }}</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.game-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  justify-content: center;
+
+  gap: 10px;
+  width: max-content;
+  margin: 0;
+  padding: 0;
+
+  div {
+    border: 1px solid black;
+    width: 80px;
+    height: 80px;
+  }
+}
+</style>
