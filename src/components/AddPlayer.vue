5d04d624e086c83c89b90810016546c57b47a6ac
feliciacalming <felicia.calming@gmail.com>
2023-04-11 15:56:55 +0200

    (feat): add AddPlayer.vue

---
+++ src/components/AddPlayer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue";
+let username = ref("");
+const emit = defineEmits(["addPlayer"]);
+</script>
+
+<template>
+  <form @submit.prevent="emit('addPlayer', username)">
+    <input
+      type="text"
+      v-model="username"
+      placeholder="skriv ditt användarnamn"
+    />
+    <button type="button"></button>
+  </form>
+</template>
+
+<style scoped lang="scss">
+input {
+  width: 30vw;
+  text-align: center;
+}
+</style>
