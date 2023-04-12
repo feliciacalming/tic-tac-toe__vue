31da0fcb8c94749fbddb08ebd5c6830aaf1c7212
feliciacalming <felicia.calming@gmail.com>
2023-04-12 14:13:21 +0200

    (feat): add GameBoard.vue

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface IPlayerSymbolProps {
+  playerSymbol: string;
+  index: number;
+}
+
+const emit = defineEmits(["switchTurns", "markSquare"]);
+defineProps<IPlayerSymbolProps>();
+</script>
+
+<template>
+  <div @click="emit('switchTurns')" :key="$props.index"></div>
+</template>
