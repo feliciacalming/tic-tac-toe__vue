1f1565205733c3acf25e385504cc6b2a2fe8ec70
feliciacalming <felicia.calming@gmail.com>
2023-04-12 14:13:45 +0200

    (refactor): update GameApp.vue

--- src/components/GameApp.vue
+++ src/components/GameApp.vue
@@ -4,13 +4,16 @@ import { Player } from "../models/Player";
 import AddPlayer from "./AddPlayer.vue";
 import { IGameState } from "../models/IGameState";
 import { saveToLocalStorage } from "../helpers/localStorage";
-import ShowGame from "./ShowGame.vue";
+import GameBoard from "./GameBoard.vue";
 
 const gameState = ref<IGameState>({
   players: [],
+  gameboard: [0, 0, 7, 5, 0, 0, 0, 0, 0],
   activePlayer: new Player(1, "", 0),
   isGameActive: true,
 });
+
+let playerSymbol = ref("");
 
 const addPlayer = (name: string) => {
   let id = 1;
@@ -24,13 +27,20 @@ const addPlayer = (name: string) => {
   saveToLocalStorage(gameState.value);
 };
 
-const switchTurns = () => {
+const switchTurns = (index: number) => {
+  gameState.value.gameboard[index] = 2;
+  console.log(JSON.parse(JSON.stringify(gameState.value.gameboard)));
+
+  console.log(index);
   if (gameState.value.activePlayer === gameState.value.players[0]) {
+    playerSymbol.value = "X";
     gameState.value.activePlayer = gameState.value.players[1];
-    return;
   } else if (gameState.value.activePlayer.playerId === 2) {
+    playerSymbol.value = "O";
     gameState.value.activePlayer = gameState.value.players[0];
   }
+
+  console.log(gameState.value.activePlayer);
 };
 </script>
 
@@ -39,11 +49,37 @@ const switchTurns = () => {
     @add-player="addPlayer"
     v-if="gameState.players.length < 2"
   ></AddPlayer>
-  <ShowGame
-    :player="gameState.activePlayer.username"
-    @toggle-player="switchTurns"
-    v-else
-  >
-    ></ShowGame
-  >
+
+  <div class="show-game" v-else>
+    <h3>Det är {{ gameState.activePlayer.username }}s tur</h3>
+    <div class="game-container">
+      <GameBoard
+        :index="index"
+        :player-symbol="playerSymbol"
+        :game="gameState"
+        v-for="(square, index) in gameState.gameboard"
+        @switch-turns="switchTurns(index)"
+      >
+      </GameBoard>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.game-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  justify-content: center;
+
+  gap: 10px;
+  width: max-content;
+  margin: 0;
+  padding: 0;
+
+  div {
+    border: 1px solid black;
+    width: 80px;
+    height: 80px;
+  }
+}
+</style>
