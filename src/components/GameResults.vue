24a38e68e28a024f5082b5ca46cc7b6a934244e4
feliciacalming <felicia.calming@gmail.com>
2023-04-13 15:05:13 +0200

    (refactor): update GameResults

--- src/components/GameResults.vue
+++ src/components/GameResults.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Player } from "../models/Player";
+
+defineProps<{ winningPlayer: Player }>();
+const emit = defineEmits(["startNewGame"]);
+</script>
 
 <template>
-  <div><span>Grattis!</span></div>
+  <div>
+    <span>Grattis {{ winningPlayer.username }}!</span>
+  </div>
+  <button @click="emit('startNewGame')">Starta ett nytt spel!</button>
+  <button>Spela en omgång till!</button>
 </template>
+
+<style scoped lang="scss">
+span {
+  font-size: 3rem;
+}
+</style>
