4f120c132327359a9fdc85fe9f3f8f9835a746ca
feliciacalming <felicia.calming@gmail.com>
2023-04-16 20:40:39 +0200

    (refactor): update addPlayer

--- src/components/AddPlayer.vue
+++ src/components/AddPlayer.vue
@@ -3,24 +3,30 @@ import { ref } from "vue";
 
 let username = ref("");
 const emit = defineEmits(["addPlayer"]);
+
+const handleSubmit = (name: string) => {
+  emit("addPlayer", name);
+  username.value = "";
+};
 </script>
 
 <template>
-  <form @submit.prevent="emit('addPlayer', username)">
-    <input
-      type="text"
-      v-model="username"
-      placeholder="skriv ditt användarnamn"
-    />
+  <form @submit.prevent="handleSubmit(username)">
+    <input type="text" v-model="username" placeholder="användarnamn" />
     <button type="button">+ Lägg till spelare</button>
   </form>
 </template>
 
 <style scoped lang="scss">
 input {
-  width: 30vw;
   text-align: center;
   background-color: white;
   border: 1px solid black;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 </style>
