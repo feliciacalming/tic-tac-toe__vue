93e147f984b951c3c9b6c34ee4f2b5d592e7b8fb
feliciacalming <felicia.calming@gmail.com>
2023-07-07 20:25:52 +0200

    (build): update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "./src/styles/variables" as var; @use "./src/styles/mixins" as mix;`,
+      },
+    },
+  },
   plugins: [vue()],
-})
+});
