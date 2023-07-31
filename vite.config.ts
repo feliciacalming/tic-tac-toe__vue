5c7cfb953de1d9fa26fe06af50c9a32be9888268
feliciacalming <felicia.calming@gmail.com>
2023-07-31 10:40:41 +0200

    (build): add base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/tic-tac-toe__vue",
   css: {
     preprocessorOptions: {
       scss: {
